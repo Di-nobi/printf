dfac1bac9e5ed369a44de591a591c45b399bdaab
Dinobi <architectstan01@gmail.com>
2022-12-10 11:58:53 -0800

    print

--- _printf.c
+++ _printf.c
@@ -1,51 +1,47 @@
 #include "main.h"
-
 /**
- * _printf - fnction to be printed in console
- * @format: Array to be checked
- * Return: number of character printed
- */
-
+* _printf - main function to print in console
+* @format: array to print and check type
+* Return: count of character printed
+**/
 int _printf(const char *format, ...)
 {
-        int num = -1;
+	int count = -1;
 
-        if (format != NULL)
-{
-        int i;
-        int (*p)(va_list);
-        va_list args;
-
-        va_start(args, format);
-
-        if (format[0] == '%' && format[1] == '\0')
-                return (-1);
-
-        num = 0;
-
-        for (i = 0; format[i] != '\0'; i++)
+	if (format != NULL)
 	{
-		if (format[i] == '%')
+		int i;
+		va_list ar_list;
+		int (*o)(va_list);
+
+		va_start(ar_list, format);
+
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+
+		count = 0;
+
+		for (i = 0; format[i] != '\0'; i++)
 		{
-			if (format[i + 1] == '%')
+			if (format[i] == '%')
 			{
-				num = num + _putchar(format[1]);
-				i++;
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i++;
+				}
+				else if (format[i + 1] != '\0')
+				{
+					o = get_func(format[i + 1]);
+					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
+					i++;
+				}
 			}
 			else
-			{
-				p = get_func(format[i + 1]);
-				if (p)
-					num = num + p(args);
-				else
-					num = _putchar(format[i]) + _putchar(format[i + 1]);
-				i++;
-			}
+				count += _putchar(format[i]);
 		}
-		else
-			num = num + _putchar(format[i]);
+		va_end(ar_list);
 	}
-	va_end(args);
-}
-return (num);
+
+	return (count);
 }
